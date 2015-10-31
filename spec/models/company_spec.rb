e62150ce5925125a5ee722c1a0a7998ad0c76b19
acareaga <aaroncareaga@gmail.com>
2015-10-31 17:02:16 -0600

    mostly through companies, need to fix routes on some views

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -15,4 +15,9 @@ RSpec.describe Company, type: :model do
     expect(company).to_not be_valid
   end
 
+  it "responds with its phone numbers after they're created" do
+    phone_number = company.phone_numbers.build(number: "333-4444")
+    expect(phone_number.number).to eq('333-4444')
+  end
+
 end
