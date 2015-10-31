6af36ef4f46a40520200296384e1e8c8a9926fc6
acareaga <aaroncareaga@gmail.com>
2015-10-31 15:41:27 -0600

    started model for a company

---
+++ spec/models/company_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe Company, type: :model do
+
+  let(:company) do
+    Company.new(name: 'Vail Resorts')
+  end
+
+  it 'is valid' do
+    expect(company).to be_valid
+  end
+
+  it 'is invalid without a name' do
+    company.name = nil
+    expect(company).to_not be_valid
+  end
+
+end
